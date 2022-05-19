d9becdf4fa69c45c5322bdc0ddeb4c6418991dd4
Piotr-Lenarczyk <piotr.lenarczyk@op.pl>
2022-05-19 17:02:46 +0200

    Initial commit

---
+++ AirportApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AirportApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
